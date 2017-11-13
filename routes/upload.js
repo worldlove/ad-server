7486b772c80f97571fcea15abea94e672ba549a1
zhangxd <zhangxd@knownsec.com>
2017-11-13 11:07:01 +0800

    修改上传文件bug

--- routes/upload.js
+++ routes/upload.js
@@ -4,21 +4,23 @@ const path = require("path");
 const upload = express.Router();
 
 // 本机测试
-// const SERVER = "http://localhost:3000";
+const SERVER = "http://localhost:3000";
 // 教室服务器地址
 // const SERVER = "http://192.168.1.210:3000";
+// 生产环境
+// const SERVER = "/";
 
 const uploadMid = multer({dest: "public/images"})
 
 upload.post("/", uploadMid.single("file"), function(req, res, next) {
-    const funcNum = req.query.CKEditorFuncNum;
     console.log("File :",req.file);
     // 为了兼容windows
     const filePath = req.file.path.replace(/\\/g, "/");
     const start = filePath.indexOf("/");
     // 生成前端可以访问到的路径；
     const subpath = filePath.substr(start);
-    const resPath = path.join(SERVER, subpath)
+    // res.send(subpath);
+    const resPath = SERVER + subpath
     res.send(resPath);
 });
 
@@ -30,7 +32,7 @@ upload.post("/ckeditor", uploadMid.single("file"), function(req, res, next) {
     const start = filePath.indexOf("/");
     // 生成前端可以访问到的路径；
     const subpath = filePath.substr(start);
-    const resPath = path.join(SERVER, subpath)
+    const resPath = SERVER + subpath
     res.send(`<script>window.parent.CKEDITOR.tools.callFunction(${funcNum}, '${resPath}')</script>`);
 });
 
