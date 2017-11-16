d6305b2eafd00db6e58d898052994ffbbdf48c13
zhangxd <zhangxd@knownsec.com>
2017-11-16 14:55:41 +0800

    修改购物车部分问题

--- routes/shoppingCart.js
+++ routes/shoppingCart.js
@@ -6,25 +6,28 @@ const cart = express.Router();
 
 cart.get("/", function(req, res, next) {
   const userId = req.session.userId;
-  // if (!userId) {
-  //   return res.status(401).send("auth failure, 验证失败，需要登录");
-  // }
-  const id = "5a04093df5bccc25355abc11";
-  UserModel.findById(userId||id).then((doc) => {
-    const products = doc.shoppingCart;
-    const getPro = products.map((pro,i) => {
+  if (!userId) {
+    return res.status(401).send("auth failure, 验证失败，需要登录");
+  }
+  UserModel.findById(userId).then((doc) => {
+    const carts = doc.shoppingCart;
+    const getPro = carts.map((pro,i) => {
       return ProductModel.findById(pro.pid).then((doc) => {
         return doc;
       })
     })
     Promise.all(getPro).then((results) => {
+      const resData = [];
       for (let i =0; i< results.length; i++) {
-        results[i].num = products[i].num
-        console.log(results[i]);
+        resData.push({
+          _id: carts[i]._id,
+          product: results[i],
+          num: carts[i].num
+        });
       }
       res.json({
         OK: true,
-        docs: results
+        docs: resData
       })
     })
   }).catch((err) => {
@@ -70,13 +73,11 @@ cart.post("/", function(req, res, next) {
 
 cart.delete("/:id", function(req, res, next) {
   const scid = req.params.id;
-  console.log(scid);
   const userId = req.session.userId;
-  // if (!userId) {
-  //   return res.status(401).send("auth failure, 验证失败，需要登录");
-  // }
-  const id = "5a04093df5bccc25355abc11";
-  UserModel.findById(userId || id).then((user) => {
+  if (!userId) {
+    return res.status(401).send("auth failure, 验证失败，需要登录");
+  }
+  UserModel.findById(userId).then((user) => {
     console.log(user.shoppingCart);
     if (user.shoppingCart.id(scid)) {
       user.shoppingCart.id(scid).remove();
